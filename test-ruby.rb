1a33a35e4c8a86d1f58ce32e5e71609e4220abfb
Tina Wuest <tina@wuest.me>
2017-08-01 12:37:38 -0500

    Update required ruby version to 2.3.1

--- test-ruby.rb
+++ test-ruby.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
-if RUBY_VERSION == "2.1.6"
+if RUBY_VERSION == "2.3.1"
   puts "Your ruby is up and running. Everything went better than expected!"
   exit 0
 else
-  puts "I'm running Ruby #{RUBY_VERSION} instead of 2.1.6. Is it installed via rbenv?"
+  puts "I'm running Ruby #{RUBY_VERSION} instead of 2.3.1. Is it installed via rbenv?"
   exit 1
 end
